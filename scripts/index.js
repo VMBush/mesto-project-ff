e950b9aed06af7bb60caa3eabbb3419f8df5ccf0
Vyacheslav Bushuev <71069537+VMBush@users.noreply.github.com>
2025-03-31 22:09:37 +0300

    adding ang deleting cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteFunction) {
+  const card = cardTemplate.cloneNode(true);
+
+  card.querySelector(".card__title").textContent = cardData.name;
+
+  const cardImage = card.querySelector(".card__image");
+  cardImage.src = cardData.link;
+  cardImage.alt =
+    "Картинка с изображением достопримечательности " + cardData.name;
+
+  const deleteButton = card.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteFunction);
+
+  return card;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+  const card = evt.target.closest(".places__item");
+  card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((el) => placesList.append(createCard(el, deleteCard)));
