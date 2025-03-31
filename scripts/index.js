1da92e6aae372698f597ab2ca6fdd753f5c95344
Vyacheslav Bushuev <71069537+VMBush@users.noreply.github.com>
2025-03-31 22:58:25 +0300

    refactor some errors from review

--- scripts/index.js
+++ scripts/index.js
@@ -6,25 +6,22 @@ const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 function createCard(cardData, deleteFunction) {
-  const card = cardTemplate.cloneNode(true);
+  const card = cardTemplate.querySelector(".places__item").cloneNode(true);
 
   card.querySelector(".card__title").textContent = cardData.name;
 
   const cardImage = card.querySelector(".card__image");
   cardImage.src = cardData.link;
-  cardImage.alt =
-    "Картинка с изображением достопримечательности " + cardData.name;
 
   const deleteButton = card.querySelector(".card__delete-button");
-  deleteButton.addEventListener("click", deleteFunction);
+  deleteButton.addEventListener("click", () => deleteCard(card));
 
   return card;
 }
 
 // @todo: Функция удаления карточки
-function deleteCard(evt) {
-  const card = evt.target.closest(".places__item");
-  card.remove();
+function deleteCard(cardElement) {
+  cardElement.remove();
 }
 
 // @todo: Вывести карточки на страницу
